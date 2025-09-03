9055ff1a746f158ea889b067135175a5872b7d78
VicenteVicente <vicente.calisto98@gmail.com>
2025-09-03 11:51:30 -0300

    use old module resolution

--- jest.config.ts
+++ jest.config.ts
@@ -1,14 +1,28 @@
-module.exports = {
+import type { Config } from 'jest';
+
+const config: Config = {
     projects: [
         {
             displayName: 'nodejs',
             preset: 'ts-jest',
             testEnvironment: 'node',
+            globals: {
+                'ts-jest': {
+                    tsconfig: 'tsconfig.json', // 👈 force Jest to use your tsconfig
+                },
+            },
         },
         {
             displayName: 'browser',
             preset: 'ts-jest',
             testEnvironment: 'jsdom',
+            globals: {
+                'ts-jest': {
+                    tsconfig: 'tsconfig.json', // 👈 force Jest to use your tsconfig
+                },
+            },
         },
     ],
 };
+
+export default config;
